f83fcbb666d7b05faeeac80fa17370c3b374d67e
Edixon Gutiérrez Mármol <exondre@gmail.com>
2025-07-25 17:25:07 -0400

    Show calculators on demand in Marks

--- src/app/features/marks/marks.component.ts
+++ src/app/features/marks/marks.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { PersonalRecordsComponent } from '../personal-records/personal-records.component';
 import { PercentageCalculatorComponent } from '../percentage-calculator/percentage-calculator.component';
 import { BumperPlatesCalculatorComponent } from '../bumper-plates-calculator/bumper-plates-calculator.component';
+import { SharedService } from '../../service/shared.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-marks',
@@ -15,4 +17,29 @@ import { BumperPlatesCalculatorComponent } from '../bumper-plates-calculator/bum
   templateUrl: './marks.component.html',
   styleUrl: './marks.component.scss',
 })
-export class MarksComponent {}
+export class MarksComponent implements OnDestroy {
+  showPercentageCalc = false;
+  showPlatesCalc = false;
+
+  private prSub: Subscription;
+  private percSub: Subscription;
+
+  constructor(private sharedService: SharedService) {
+    this.prSub = this.sharedService
+      .getSelectedPREvent()
+      .subscribe(() => {
+        this.showPercentageCalc = true;
+        this.showPlatesCalc = false;
+      });
+    this.percSub = this.sharedService
+      .getSelectedPercentageEvent()
+      .subscribe(() => {
+        this.showPlatesCalc = true;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.prSub.unsubscribe();
+    this.percSub.unsubscribe();
+  }
+}
