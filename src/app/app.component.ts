dcaac56b78f55f5f93a6095d12de3b63a506d0bd
Edixon Gutiérrez Mármol <exondre@gmail.com>
2025-07-25 16:43:17 -0400

    hide header on home and center branding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,15 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  hideHeader = false;
+
+  constructor(private router: Router) {
+    this.hideHeader = this.router.url === '/home';
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe((e) => {
+        this.hideHeader = e.urlAfterRedirects === '/home';
+      });
+  }
+}
